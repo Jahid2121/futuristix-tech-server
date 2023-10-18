ddf67585cf9b3cab0d0c9823d1e97dd645f042ac
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-18 08:57:47 +0600

    Dotenv config updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -10,12 +11,12 @@ app.use(cors())
 app.use(express.json())
 
 
-// TechTuber 
-// FmAFsFst20YmlJAB
 
 
 
-const uri = "mongodb+srv://TechTuber:FmAFsFst20YmlJAB@cluster0.pu45iww.mongodb.net/?retryWrites=true&w=majority";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu45iww.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
