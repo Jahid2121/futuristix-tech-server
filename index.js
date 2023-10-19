86c22a3be64ea5068a8ef4ddd558137ff4fbf3a6
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-19 08:48:16 +0600

    fetch single data through id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
@@ -34,6 +34,13 @@ async function run() {
     app.get('/products', async(req,res) => {
       const cursor = productCollection.find()
       const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
       res.send(result)
     })
 
